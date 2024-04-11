ed37ef577429814f4442c6321cc1abd65f22e8e3
Sorawit <2110511106002@live4.utcc.ac.th>
2024-04-12 04:30:02 +0700

    update

--- database.sql
+++ database.sql
@@ -285,13 +285,13 @@ CREATE TABLE IF NOT EXISTS payment_info (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS course_reviews (
+CREATE TABLE reviews (
     id INT AUTO_INCREMENT PRIMARY KEY,
     course_id INT NOT NULL,
     username VARCHAR(255) NOT NULL,
     trainer_id INT NOT NULL,
     history_id INT NOT NULL,
     rating INT NOT NULL,
-    review TEXT NOT NULL,
+    review TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
